4e8e37e6fd3d03ecfd1ad5468e885f41aa7bed26
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-02 02:41:53 -0700

    updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree
  *
- * Return: Height of the tree, or 0 if tree is NULL
+ * Return: Height of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -13,15 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/* Recursively calculate the height of the left subtree */
 	left_height = binary_tree_height(tree->left);
-
-	/* Recursively calculate the height of the right subtree */
 	right_height = binary_tree_height(tree->right);
 
-	/* Return the maximum of the two heights, plus 1 for the current node */
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	/* Return the height of the taller subtree + 1 (current node) */
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
