ea42a046b620bade964581c5376ed5a9fde12f64
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-02 04:46:03 -0700

    updated

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,46 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Find the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- * Return: Pointer to the lowest common ancestor node, or NULL if not found
+ * binary_trees_ancestor - function that checks an ancestor
+ * @first: First node
+ * @second: Second node
+ * Return: the node of the ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-	if (!first || !second)
+	binary_tree_t *p, *q;
+
+	if (first == NULL || second == NULL)
+	{
 		return (NULL);
-
-	/* Create arrays to store paths from root to each node */
-	binary_tree_t *path1[1024]; /* Adjust the size as needed */
-	binary_tree_t *path2[1024];
-	size_t len1 = 0, len2 = 0;
-
-	/* Find the path from root to first node */
-	while (first)
+	}
+	if (first == second)
 	{
-		path1[len1++] = (binary_tree_t *)first;
-		first = first->parent;
+		return ((binary_tree_t *)first);
 	}
 
-	/* Find the path from root to second node */
-	while (second)
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
 	{
-		path2[len2++] = (binary_tree_t *)second;
-		second = second->parent;
+		return (binary_trees_ancestor(first, q));
 	}
-
-	/* Compare the paths to find the last common node */
-	while (len1 > 0 && len2 > 0 && path1[len1 - 1] == path2[len2 - 1])
+	else if (q == NULL || p == second || (!q->parent && p))
 	{
-		len1--;
-		len2--;
+		return (binary_trees_ancestor(p, second));
 	}
-
-	/* If no common ancestor was found, return NULL */
-	if (len1 == 0 || len2 == 0)
-		return (NULL);
-
-	/* Return the last common ancestor */
-	return (path1[len1]);
+	return (binary_trees_ancestor(p, q));
 }
